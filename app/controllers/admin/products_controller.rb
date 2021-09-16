723089d4b9e4fe848aec9cbd0a730691a63acbac
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-09-16 13:45:41 +0000

    fixed admin authentication bug

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductsController < ApplicationController
-
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTHENTICATION_USERNAME"], password: ENV["HTTP_BASIC_AUTHENTICATION_PASSWORD"]
   def index
     @products = Product.order(id: :desc).all
   end
